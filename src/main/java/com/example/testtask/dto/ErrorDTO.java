ae56848460a7ad4aaf1994d1bc5660244c83f0f6
Alex <alextetekin@gmail.com>
2024-03-27 08:32:38 +0300

    Tetstask

---
+++ src/main/java/com/example/testtask/dto/ErrorDTO.java
@@ -0,0 +1,10 @@
+package com.example.testtask.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class ErrorDTO {
+    private final String message;
+}
